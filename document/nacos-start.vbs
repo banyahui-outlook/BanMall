5a9536cd5d3d5fbaeec61571414eb0693575a156
banyahui0425 <banyahui@outlook.com>
2021-11-13 17:38:23 +0800

    change server

---
+++ document/nacos-start.vbs
@@ -0,0 +1,2 @@
+set ws=WScript.CreateObject("WScript.Shell") 
+ws.Run "D:\Temp\Byhui\Study\Install\nacos-server-2.0.3\bin\startup.cmd /start",0
---
+++ document/sentinel-start.bat
@@ -0,0 +1,3 @@
+@echo off
+start javaw -Dserver.port=8868 -Duser.home=D:\Temp\Byhui\Study\Install\sentinel-1.8.2 -Dproject.name=by-sentinel -jar sentinel-dashboard-1.8.2.jar
+exit
