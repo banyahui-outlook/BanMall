b8fe949d0813fb8c1a8d5c0376d77ae9adebd989
bancloud0425 <bancloud@@outlook.com>
2021-11-15 22:01:05 +0800

    sentinel faild

--- back_end/cloud-web/src/main/java/com/aj/handler/SentinelHandlersClass.java
+++
@@ -1,9 +0,0 @@
-package com.aj.handler;
-
-import com.alibaba.csp.sentinel.slots.block.BlockException;
-
-public class SentinelHandlersClass {
-    public static String limitBlockHandler(BlockException be) {
-        return "block a1........";
-    }
-}
---
+++ back_end/cloud-web/src/main/java/com/aj/handler/TestService.java
@@ -0,0 +1,9 @@
+package com.aj.handler;
+
+public interface TestService {
+    void test();
+
+    String hello(long s);
+
+    String helloAnother(String name);
+}
