734fef0b6e2ba5ac327ea7df73f51673be9e0a87
bancloud0425 <bancloud@@outlook.com>
2021-11-19 23:01:40 +0800

    docker compose test

--- document/nacos-start.vbs
+++
@@ -1,2 +0,0 @@
-set ws=WScript.CreateObject("WScript.Shell") 
-ws.Run "D:\Temp\Byhui\Study\Install\nacos-server-2.0.3\bin\startup.cmd /start",0
--- document/sentinel-start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start javaw -Dserver.port=8868 -Duser.home=D:\Temp\Byhui\Study\Install\sentinel-1.8.2 -Dproject.name=by-sentinel -jar sentinel-dashboard-1.8.2.jar
-exit
